4ac92dbf3d021c7355bfe5f5ff6d4497abb82bda
valdofal <v.dofal@gmail.com>
2020-10-13 12:37:45 +0200

    e04.js

--- onecode/season.01/main.js
+++ onecode/season.01/main.js
@@ -1,6 +1,7 @@
 const e01 = require('./e01')
 const e02 = require('./e02.js')
 const e03 = require('./e03')
+const e04 = require('./e04')
 
     //e01 :
 
@@ -13,5 +14,10 @@ const e03 = require('./e03')
 
     //e03:
 
-nametxt = process.argv.slice(2).toString()
-e03(nametxt)
+//nametxt = process.argv.slice(2).toString()
+//e03(nametxt)
+
+    //e04
+
+nameTxtTwo = process.argv.slice(2).toString()
+e04(nameTxtTwo)
